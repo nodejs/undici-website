960830f01e1599a1dc5e1d1c73752b9c56f99119
Mert Can Altin <mert.altin@trendyol.com>
2024-05-13 16:02:33 +0300

    feat: centers routes elements in navbar

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -10,46 +10,40 @@ export default function Navbar() {
     const pathname = usePathname();
     const routes = [
         {
-            text: "undici",
-            href: "/"
-        },
-        {
             text: "docs",
             href: "/docs"
         },
         {
             text: "benchmarks",
             href: "/benchmarks"
+        },
+        {
+            text: "examples",
+            href: "/examples"
         }
     ];
     return (
         <header>
-            <nav
-                className="flex items-center justify-between border-b-2 border-green-700 px-2 py-4 font-semibold text-gray-900"
-                aria-label="Global navigation"
-            >
-                <div className="flex gap-4">
-                    {
-                        routes.map(route => (
-                            <Link key={route.text} href={route.href} className={pathname === route.href ? "text-green-700": ""}>
-                                {route.text}
-                            </Link>
-                        ))
-                    }
+            <nav className="flex items-center justify-between border-b-2 border-green-700 px-2 py-4 font-semibold text-gray-900" aria-label="Global navigation">
+                <div className="flex items-center gap-4">
+                    <span>undici</span>
                 </div>
-                <div className="flex-end flex gap-4 items-center">
-                    <span className="text-2xl">
-                        <Link href="https://twitter.com/nodejs" target="_blank">
-                            <Image className="h-5 w-5" src={TwitterDark} alt="Twitter Logo" />
+                <div className="flex items-center justify-center gap-4">
+                    {routes.map(route => (
+                        <Link key={route.text} href={route.href} className={pathname === route.href ? "text-green-700": ""}>
+                            {route.text}
                         </Link>
-                    </span>
-                    <span className="text-2xl">
-                        <Link href="https://github.com/nodejs/undici" target="_blank">
-                            <Image className="h-6 w-6" src={GitHubLogoDark} alt="GitHub Logo" />
-                        </Link>
-                    </span>
+                    ))}
+                </div>
+                <div className="flex gap-4 items-center">
+                    <Link href="https://twitter.com/nodejs" target="_blank">
+                        <Image className="h-5 w-5" src={TwitterDark} alt="Twitter Logo" />
+                    </Link>
+                    <Link href="https://github.com/nodejs/undici" target="_blank">
+                        <Image className="h-6 w-6" src={GitHubLogoDark} alt="GitHub Logo" />
+                    </Link>
                 </div>
             </nav>
         </header>
-    )
+    );
 }
